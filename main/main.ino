5bb6c9032372efc58e5fa6986871375929cce1d5
PAWAN G <140328953+PAWAN-G14@users.noreply.github.com>
2023-08-27 13:51:24 +0530

    Added info

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,24 @@
+/*
+--------------------------------------------
+Simple TicTacToe AI using Minimax algorithm.
+--------------------------------------------
+
+............................................
+Runs on an Arduino (UNO in this case),using
+Serial monitor as the interface.
+............................................
+
+ - - - - - - - - - - - - - - - - - - - - 
+ Author: Pawan G.
+ Status: 14 y/o still learning.
+ - - - - - - - - - - - - - - - - - - - - 
+
+NOTE: This code is undocumeted to keep up the minimalism of this branch.
+      For documented code, please, go refer the main repo at https://github.com/PAWAN-G14/tictactoe-arduino-serial.
+      In the main branch, checkout /src for main.ino and /include for AI.h, Game.h and Board.h
+
+*/
+
 #include "Game.h"
 
 void setup() { Serial.begin(9600); }
