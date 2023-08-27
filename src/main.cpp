577e51159eab09c3cdcc203f9abb280ea0917473
PAWAN G <140328953+PAWAN-G14@users.noreply.github.com>
2023-08-27 12:02:40 +0530

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
+/*
+--------------------------------------------
+Simple TicTacToe AI using Minimax algorithm.
+--------------------------------------------
+
+............................................
+Runs on an Arduino (UNO in this case),using
+Serial monitor as the interface.
+............................................
+
+ - - - - - - - - - - - - - - - - - - - - 
+ Author: Pawan G.
+ Status: 14 y/o still learning.
+ - - - - - - - - - - - - - - - - - - - - 
+
+*/
+
 #include <Arduino.h>
 #include <Game.h>
 
 void setup() {
-
 	// Setting up the serial monitor @9600 baud rate
 	Serial.begin(9600); 
-
 }
 
 void loop() {
